7a503a1d6e13d94adc59405beda02a3e4983bce7
MacysGitHub <macy.gm@gmail.com>
2023-01-05 00:09:14 -0700

    Added memory scan
    VirtualQueryEx() now scans page by page for given process.

--- MemoryHacker/OpenProcessByName.cpp
+++ MemoryHacker/OpenProcessByName.cpp
@@ -7,155 +7,151 @@
 #include <minidumpapiset.h>
 
 
-DWORD FindTargetProcess(DWORD dwProcessID, wchar_t* processName) {
-    HANDLE hProcess;
-    BOOL bEnumProcessModule = FALSE;
-    HMODULE hMod;
-    DWORD bytesneeded = 0;
-    wchar_t chProcessName[MAX_PATH] = L"UNKNOWN";
-    wchar_t chProcessPath[MAX_PATH] = L"UNKNOWN PATH";
-    hProcess = OpenProcess(PROCESS_ALL_ACCESS,
-        FALSE, dwProcessID);
-    if (NULL != hProcess) {
-        bEnumProcessModule = K32EnumProcessModulesEx(hProcess,
-            &hMod,
-            sizeof(hMod),
-            &bytesneeded,
-            LIST_MODULES_ALL);
-        if (bEnumProcessModule == TRUE) {
-            K32GetModuleBaseNameW(hProcess,
-                hMod,
-                chProcessName,
-                sizeof(chProcessName) / sizeof(wchar_t));
-            
-            //std::wcout << "Process Name: " << chProcessName << std::endl;
-            if (_wcsicmp(chProcessName, processName) == 0) {
-                return dwProcessID;
-            }
-            K32GetModuleFileNameExW(
-                hProcess,
-                hMod,
-                chProcessPath,
-                sizeof(chProcessPath) / sizeof(wchar_t));
-            //std::wcout << "Process Path: " << chProcessPath << std::endl;
-        }
-    }
+DWORD FindTargetProcess(DWORD dwProcessID, wchar_t* processName)
+{
+	HANDLE hProcess;
+	BOOL bEnumProcessModule = FALSE;
+	HMODULE hMod;
+	DWORD bytesneeded = 0;
+	wchar_t chProcessName[MAX_PATH] = L"UNKNOWN";
+	wchar_t chProcessPath[MAX_PATH] = L"UNKNOWN PATH";
+	hProcess = OpenProcess(PROCESS_ALL_ACCESS,
+		FALSE, dwProcessID);
+	if (NULL != hProcess) {
+		bEnumProcessModule = K32EnumProcessModulesEx(hProcess,
+			&hMod,
+			sizeof(hMod),
+			&bytesneeded,
+			LIST_MODULES_ALL);
+		if (bEnumProcessModule == TRUE) {
+			K32GetModuleBaseNameW(hProcess,
+				hMod,
+				chProcessName,
+				sizeof(chProcessName) / sizeof(wchar_t));
+
+			//std::wcout << "Process Name: " << chProcessName << std::endl;
+			if (_wcsicmp(chProcessName, processName) == 0) {
+				return dwProcessID;
+			}
+			K32GetModuleFileNameExW(
+				hProcess,
+				hMod,
+				chProcessPath,
+				sizeof(chProcessPath) / sizeof(wchar_t));
+			//std::wcout << "Process Path: " << chProcessPath << std::endl;
+		}
+	}
 }
 
-HANDLE OpenProcessByName(wchar_t* processName) {
-    PROCESSENTRY32 entry;
-    entry.dwSize = sizeof(PROCESSENTRY32);
-    BOOL enumbool = FALSE;
-    DWORD processIds[2048] = { 0 };
-    DWORD bytesneeded = 0;
-    DWORD TotalProcesses = 0;
-    DWORD targetProcess = 0;
-    
+HANDLE OpenProcessByName(wchar_t* processName)
+{
+	PROCESSENTRY32 entry;
+	HANDLE proc = NULL;
+	entry.dwSize = sizeof(PROCESSENTRY32);
+	BOOL enumbool = FALSE;
+	DWORD processIds[2048] = { 0 };
+	DWORD bytesneeded = 0;
+	DWORD TotalProcesses = 0;
+	DWORD targetProcess = 0;
 
-    enumbool = K32EnumProcesses(processIds, 
-        sizeof(processIds), 
-        &bytesneeded);
 
-    if (enumbool == TRUE) {
-        TotalProcesses = bytesneeded / sizeof(DWORD);
-        std::cout << "Total Processes Running: " << TotalProcesses << std::endl;
-    }
+	enumbool = K32EnumProcesses(processIds,
+		sizeof(processIds),
+		&bytesneeded);
 
-    for (size_t i = 0; i < TotalProcesses; i++) {
-        targetProcess = FindTargetProcess(processIds[i], processName);
-        HANDLE proc = NULL;
-        wchar_t name[MAX_PATH];
-        DWORD obtainedName;
+	if (enumbool == TRUE)
+	{
+		TotalProcesses = bytesneeded / sizeof(DWORD);
+		std::cout << "Total Processes Running: " << TotalProcesses << std::endl;
+	}
 
-        
-        //Get Process List and Open Handle to Process
-        proc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, targetProcess);
-        
-        if (proc != NULL) {
-            std::cout << "Successfully found and opened handle to process!" << std::endl;
-            obtainedName = K32GetProcessImageFileNameW(proc, name, sizeof(name));
-            std::cout << "processid: " << targetProcess << std::endl;
-            std::wcout << "Sanity Check: " << name << std::endl;
-            HMODULE modbase = {0};
-            MODULEINFO modInfo;
-            MEMORY_BASIC_INFORMATION pmembi = { 0 };
-            BOOL retMod;
-            size_t progSize;
-            retMod = K32GetModuleInformation(proc, modbase, &modInfo, sizeof(MODULEINFO));
-            LPVOID entry = modInfo.EntryPoint;
-            size_t pageSize;
+	for (size_t i = 0; i < TotalProcesses; i++)
+	{
+		targetProcess = FindTargetProcess(processIds[i], processName);
 
-            //Get Page Size for Program
-            pageSize = VirtualQueryEx(proc, entry, &pmembi, sizeof(MEMORY_BASIC_INFORMATION));
-            
-            if (pageSize == 0) {
-                std::cout << "Error!: " << GetLastError();
-            }
+		wchar_t name[MAX_PATH];
+		DWORD obtainedName;
 
-            std::cout << "Region Size: " << pmembi.RegionSize;
 
-            PROCESS_MEMORY_COUNTERS pmc;
-            K32GetProcessMemoryInfo(proc, &pmc, sizeof(pmc));
+		//Get Process List and Open Handle to Process
+		proc = OpenProcess(PROCESS_ALL_ACCESS, FALSE, targetProcess);
 
-           
+		if (proc != NULL)
+		{
+			std::cout << "Successfully found and opened handle to process!" << std::endl;
+			obtainedName = K32GetProcessImageFileNameW(proc, name, sizeof(name));
+			std::cout << "processid: " << targetProcess << std::endl;
+			std::wcout << "Sanity Check: " << name << std::endl;
+			HMODULE modbase = { 0 };
+			MODULEINFO modInfo;
+			MEMORY_BASIC_INFORMATION pmembi = { 0 };
+			BOOL retMod;
+			size_t progSize;
+			retMod = K32GetModuleInformation(proc, modbase, &modInfo, sizeof(MODULEINFO));
+			LPVOID entry = modInfo.EntryPoint;
+			size_t pageSize = 0;
 
-            /*OFSTRUCT ofs = { 0 };
-            HFILE hFile = OpenFile("C:\\ProcDump.bin", &ofs, OF_CREATE);
-            PMINIDUMP_EXCEPTION_INFORMATION miniException = { 0 };
-            PMINIDUMP_USER_STREAM_INFORMATION miniStream = { 0 };
-            PMINIDUMP_CALLBACK_INFORMATION callBack = { 0 };
+			//Get Page Size for Program
+			/*pageSize = VirtualQueryEx(proc, entry, &pmembi, sizeof(MEMORY_BASIC_INFORMATION));
 
-            BOOL dumped = MiniDumpWriteDump(proc, targetProcess, &hFile, MiniDumpWithFullMemory, miniException, miniStream, callBack);
+			if (pageSize == 0) {
+				std::cout << "Error!: " << GetLastError() << std::endl;
+			}
 
-            std::cout << "Dump success?: " << dumped << std::endl;
-            if (!dumped) {
-                std::cout << "MiniDump Fail: " << GetLastError() << std::endl;
-            }*/
-            //SYSTEM_INFO sysInfo;
-            //GetSystemInfo(&sysInfo);
-            //LPVOID procMem[4096];
-            //std::cout << "Page Size: " << sysInfo.dwPageSize << std::endl;
-            //std::cout << "protected? " << pmembi.Protect << std::endl;
-            //ReadProcessMemory(proc, entry, procMem, sysInfo.dwPageSize, &progSize);
-            ////Determine 
-            //std::cout << "mem page size: " << pmembi.RegionSize << std::endl;
-            //for (int i = 0; i < ((sysInfo.dwPageSize/8)); i++) {
-            //    //std::cout << procMem[i] << std::endl;
-            //    i++;
-            //}
-        }
-    }
+			std::cout << "Region Size: " << pmembi.RegionSize << std::endl;*/
 
-    //HANDLE hProcess = new HANDLE;
-    //HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
-    //DWORD procId;
-    //HMODULE hMod = { 0 };
-    //LPWSTR filename = { 0 };
+			PROCESS_MEMORY_COUNTERS pmc;
+			K32GetProcessMemoryInfo(proc, &pmc, sizeof(pmc));
 
-    //if (Process32First(snapshot, &entry) == TRUE)
-    //{
-    //    while (Process32Next(snapshot, &entry) == TRUE)
-    //    {
-    //        std::cout << "Scanning process..." << std::endl;
-    //        if (_wcsicmp(entry.szExeFile, L"Notepad.exe"))
-    //        {
-    //            hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, entry.th32ProcessID);
-    //            std::cout << "Process Id: " << entry.th32ProcessID << std::endl;
-    //            //CloseHandle(hProcess); We'll close in other part of our program
-    //        }
-    //    }
-    //}
+			MEMORY_BASIC_INFORMATION meminfo;
+			char* addr = 0;
 
-    //CloseHandle(snapshot);
 
-    //if (hProcess == NULL) {
-    //    return INVALID_HANDLE_VALUE;
-    //    CloseHandle(hProcess);
-    //}
-    //else {
-    //    return hProcess;
-    //    CloseHandle(hProcess);
-    //}
-    //return 0;
-    return 0;
+			MEMORY_BASIC_INFORMATION mbi;
+
+			char buffer[0x1000];
+
+			while (VirtualQueryEx(proc, addr, &mbi, sizeof(mbi)))
+			{
+
+				std::cout << "Protect? " << mbi.Protect << std::endl;
+				std::cout << "state? " << mbi.State << std::endl;
+
+				if (mbi.State != MEM_COMMIT || mbi.Protect == PAGE_NOACCESS)
+				{
+					char* buffer = new char[mbi.RegionSize];
+
+					ReadProcessMemory(proc, addr, buffer, mbi.RegionSize, nullptr);
+					std::cout << "data: " << (char)buffer << std::endl;
+				}
+				addr += mbi.RegionSize;
+			}
+
+			CloseHandle(proc);
+
+			//	HANDLE dump_file = CreateFile("dump.dmp", GENERIC_WRITE, FILE_SHARE_WRITE, nullptr, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, nullptr);
+			//	if (dump_file == INVALID_HANDLE_VALUE) {
+			//		std::cerr << "Error creating dump file: " << GetLastError() << std::endl;
+			//	}
+
+			//	//Create a MINIDUMP_EXCEPTION_INFORMATION structure to describe the exception that occurred, if any.
+			//	MINIDUMP_EXCEPTION_INFORMATION exception_info;
+			//	EXCEPTION_POINTERS ep;
+			//	exception_info.ThreadId = GetCurrentThreadId();
+			//	exception_info.ExceptionPointers = GetExceptionInformation();
+			//	exception_info.ClientPointers = TRUE;
+
+			//	// Call MiniDumpWriteDump to create the minidump.
+			//	BOOL success = MiniDumpWriteDump(proc, targetProcess, dump_file, MiniDumpWithFullMemory, &exception_info, nullptr, nullptr);
+			//	if (!success) {
+			//		std::cerr << "Error writing minidump: " << GetLastError() << std::endl;
+
+			//	}
+			//	
+			//	std::cout << "Minidump created successfully!" << std::endl;
+
+			//}
+			return proc;
+		}
+	}
 }
